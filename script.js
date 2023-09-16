c8f835fec9c8fd680b8ff4beccf3431c7a4e0297
SM-junior <shahinalam12341@gmail.com>
2023-09-17 02:37:13 +0600

    fetching done

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
 //Simple Calculator
-const calculatorForm=document.querySelector('.calculator-form');
-const firstNumber=document.querySelector('#first-number');
-const secondNumber=document.querySelector('#second-number');
-const btn=document.querySelector('.calculate-btn');
-const calculation=document.querySelector('.calculation')
+const calculatorForm = document.querySelector('.calculator-form');
+const firstNumber = document.querySelector('#first-number');
+const secondNumber = document.querySelector('#second-number');
+const btn = document.querySelector('.calculate-btn');
+const calculation = document.querySelector('.calculation')
 
-const calculate=(e)=>{
+const calculate = (e) => {
     e.preventDefault();
-    calculation.style.display='block';
-    const add=parseInt(firstNumber.value)+parseInt(secondNumber.value);
-    const sub=parseInt(firstNumber.value)-parseInt(secondNumber.value);
-    const mul=parseInt(firstNumber.value)*parseInt(secondNumber.value);
-    const div=parseInt(firstNumber.value)/parseInt(secondNumber.value);
-    const finalDiv=div.toFixed(2)
-    document.querySelector('.add').innerHTML=`${firstNumber.value} + ${secondNumber.value}= ${add}`;
-    document.querySelector('.sub').innerHTML=`${firstNumber.value} - ${secondNumber.value}= ${sub}`;
-    document.querySelector('.mul').innerHTML=`${firstNumber.value} x ${secondNumber.value}= ${mul}`;
-    document.querySelector('.div').innerHTML=`${firstNumber.value} / ${secondNumber.value}= ${finalDiv}`;
+    calculation.style.display = 'block';
+    const add = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+    const sub = parseInt(firstNumber.value) - parseInt(secondNumber.value);
+    const mul = parseInt(firstNumber.value) * parseInt(secondNumber.value);
+    const div = parseInt(firstNumber.value) / parseInt(secondNumber.value);
+    const finalDiv = div.toFixed(2)
+    document.querySelector('.add').innerHTML = `${firstNumber.value} + ${secondNumber.value}= ${add}`;
+    document.querySelector('.sub').innerHTML = `${firstNumber.value} - ${secondNumber.value}= ${sub}`;
+    document.querySelector('.mul').innerHTML = `${firstNumber.value} x ${secondNumber.value}= ${mul}`;
+    document.querySelector('.div').innerHTML = `${firstNumber.value} / ${secondNumber.value}= ${finalDiv}`;
     e.target.reset();
 
 }
 calculatorForm.addEventListener('submit', calculate)
 
 //Simple Todo
-const todoForm=document.querySelector('.todo-form');
-const todoInput=document.querySelector('#todo-input');
-const todoList=document.querySelector('.todo-list');
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('.todo-list');
 
-const addTodo=(e)=>{
+const addTodo = (e) => {
     e.preventDefault();
-    const ul=document.createElement('ul');
-    ul.innerHTML +=`<li class="border-solid border-2 border-indigo-600 w-[50%] mx-auto mb-2 py-1 px-3 flex justify-between">${todoInput.value} <span><i class="fa-solid fa-trash delete hover:scale-150 hover:text-red-600 hover:duration-700"></i></span></li>`
+    const ul = document.createElement('ul');
+    ul.innerHTML += `<li class="border-solid border-2 border-indigo-600 w-[50%] mx-auto mb-2 py-1 px-3 flex justify-between">${todoInput.value} <span><i class="fa-solid fa-trash delete hover:scale-150 hover:text-red-600 hover:duration-700"></i></span></li>`
     todoList.appendChild(ul);
     e.target.reset();
 }
-
-
-
-const deleteTodo=(e)=>{
-    if(e.target.classList.contains('delete')){
+const deleteTodo = (e) => {
+    if (e.target.classList.contains('delete')) {
         e.target.parentElement.parentElement.remove();
     }
 }
@@ -46,22 +43,43 @@ todoForm.addEventListener('submit', addTodo);
 todoList.addEventListener('click', deleteTodo)
 
 //Simple Temperature Converter
-const temperatureForm=document.querySelector('.temperature-form');
-const celsiusInput=document.querySelector('#celsius-input');
-const temp=document.querySelector('.temp');
-const fahren=document.querySelector('.fah')
-const kelvin=document.querySelector('.kel')
+const temperatureForm = document.querySelector('.temperature-form');
+const celsiusInput = document.querySelector('#celsius-input');
+const temp = document.querySelector('.temp');
+const fahren = document.querySelector('.fah')
+const kelvin = document.querySelector('.kel')
 
-const convertTemp=(e)=>{
+const convertTemp = (e) => {
     e.preventDefault();
-    const fah=(parseInt(celsiusInput.value)*(9/5))+32;
-    const kel=parseInt(celsiusInput.value)+273.15;
+    const fah = (parseInt(celsiusInput.value) * (9 / 5)) + 32;
+    const kel = parseInt(celsiusInput.value) + 273.15;
     temp.classList.add('text-center')
-    fahren.innerHTML=`${celsiusInput.value} degree Celsius = ${fah}<span class='text-lg'>F</span>`;
-    kelvin.innerHTML=`${celsiusInput.value} degree Celsius = ${kel}<span class='text-lg'>K</span>`;
+    fahren.innerHTML = `${celsiusInput.value} degree Celsius = ${fah}<span class='text-lg'>F</span>`;
+    kelvin.innerHTML = `${celsiusInput.value} degree Celsius = ${kel}<span class='text-lg'>K</span>`;
     e.target.reset()
 }
-
 temperatureForm.addEventListener('submit', convertTemp)
 
+//Simple API fetching
 
+const url = "https://jsonplaceholder.typicode.com/users?_limit=8";
+const displayUser = async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    data.map(item => {
+        const fetchInfo = document.querySelector('.fetch-info');
+        fetchInfo.innerHTML += `
+        <div class="card card-compact w-76 bg-base-100 shadow-xl">
+        <figure><img src='https://i.ibb.co/x8Q0BLV/P3-OLGJ1-copy-1.png' alt="Shoes" /></figure>
+        <div class="card-body">
+        <h2 class="card-title">Name: ${item.name}</h2>
+        <p>Email: ${item.email}</p>
+        <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+        </div>
+        </div>
+        </div>
+        `
+    })
+}
+displayUser();
